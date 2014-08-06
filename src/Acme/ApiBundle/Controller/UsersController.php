a9f2d6893796fc0c114a157fbc3e8aab6be31781
davide cavaliere <cavaliere.davide@gmail.com>
2014-08-06 22:57:13 +0100

    implementing user controller

--- src/Acme/ApiBundle/Controller/UsersController.php
+++ src/Acme/ApiBundle/Controller/UsersController.php
@@ -7,8 +7,52 @@
  */
 
 namespace Acme\ApiBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @Route("/users")
+ *
+ */
+class UsersController extends Controller {
 
-class UsersController {
+    /**
+     * @Route("/")
+     * @Method("GET")
+     */
+    public function getUsers() {
+
+        $repository = $this->getDoctrine()->getRepository("AcmeApiBundle:User");
+
+        $users = $repository->findAll();
+
+        return new JsonResponse($users);
+    }
+
+    /**
+     * @Route("/add")
+     * @Method("POST")
+     */
+    public function saveUser(Request $request) {
+        var_dump($request);
+
+        return new JsonResponse();
+    }
+
+    /**
+     * @Route("/{id}")
+     */
+    public function getUserById($id) {
+        $repository = $this->getDoctrine()->getRepository("AcmeApiBundle:User");
+
+        $user = $repository->find($id);
+
+        return new JsonResponse($user);
+    }
+
 
 } 
