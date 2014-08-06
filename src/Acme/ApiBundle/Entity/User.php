931bb3821c63591b0d92025374144c93fa79b564
davide cavaliere <cavaliere.davide@gmail.com>
2014-08-06 21:40:19 +0100

    generating entities

---
+++ src/Acme/ApiBundle/Entity/User.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Acme\ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Acme\ApiBundle\Entity\UserRepository")
+ */
+class User
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="alias", type="string", length=255)
+     */
+    private $alias;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="pin", type="integer")
+     */
+    private $pin;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set alias
+     *
+     * @param string $alias
+     * @return User
+     */
+    public function setAlias($alias)
+    {
+        $this->alias = $alias;
+
+        return $this;
+    }
+
+    /**
+     * Get alias
+     *
+     * @return string 
+     */
+    public function getAlias()
+    {
+        return $this->alias;
+    }
+
+    /**
+     * Set pin
+     *
+     * @param integer $pin
+     * @return User
+     */
+    public function setPin($pin)
+    {
+        $this->pin = $pin;
+
+        return $this;
+    }
+
+    /**
+     * Get pin
+     *
+     * @return integer 
+     */
+    public function getPin()
+    {
+        return $this->pin;
+    }
+}
