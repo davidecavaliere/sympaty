0b4802cd78d6091df65e0656ade032e1e2f49cde
davide cavaliere <cavaliere.davide@gmail.com>
2014-08-06 22:56:15 +0100

    refactoring Default Controller

--- src/Acme/ApiBundle/Controller/DefaultController.php
+++ src/Acme/ApiBundle/Controller/DefaultController.php
@@ -5,15 +5,24 @@ namespace Acme\ApiBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return array('name' => 'bello');
+    }
+
+    /**
+     * @Route("echo/{what}")
+     */
+    public function echoAction($what)
+    {
+        return new Response($what);
     }
 }
