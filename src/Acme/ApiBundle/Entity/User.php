4df53c877d2d5a866ce3eacf64e56ba7d84c464f
davide cavaliere <cavaliere.davide@gmail.com>
2014-08-06 22:56:41 +0100

    user entity is now serializable

--- src/Acme/ApiBundle/Entity/User.php
+++ src/Acme/ApiBundle/Entity/User.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="Acme\ApiBundle\Entity\UserRepository")
  */
-class User
+class User implements \JsonSerializable
 {
     /**
      * @var integer
@@ -90,5 +90,17 @@ class User
     public function getPin()
     {
         return $this->pin;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.4.0)<br/>
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     */
+    public function jsonSerialize()
+    {
+        return get_object_vars($this);
     }
 }
