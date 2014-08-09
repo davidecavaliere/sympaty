eb65af94fcc36e750056738da9b9af3845a7ca49
davide cavaliere <cavaliere.davide@gmail.com>
2014-08-09 15:28:46 +0100

    implenting userService

--- src/Acme/ApiBundle/Service/UserService.php
+++ src/Acme/ApiBundle/Service/UserService.php
@@ -9,6 +9,64 @@
 namespace Acme\ApiBundle\Service;
 
 
+use Acme\ApiBundle\Entity\User;
+use Acme\ApiBundle\Entity\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\ClassMetadata;
+
 class UserService {
+
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var UserRepository
+     */
+    protected $rep;
+
+    public function __construct(EntityManager $em) {
+        $this->setEm($em);
+    }
+
+    public function saveUser(User $user) {
+        $this->em->persist($user);
+        $this->em->flush($user);
+
+        return $user;
+    }
+
+    /**
+     * @return \Acme\ApiBundle\Entity\UserRepository
+     */
+    public function getRep()
+    {
+        return $this->rep;
+    }
+
+    /**
+     * @param \Acme\ApiBundle\Entity\UserRepository $rep
+     */
+    public function setRep($rep)
+    {
+        $this->rep = $rep;
+    }
+
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
+    public function getEm()
+    {
+        return $this->em;
+    }
+
+    /**
+     * @param \Doctrine\ORM\EntityManager $em
+     */
+    public function setEm($em)
+    {
+        $this->em = $em;
+    }
 
 } 
